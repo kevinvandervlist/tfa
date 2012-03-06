adfb3bdaa4397145c13465e598ccdc5437d6665b
Kevin van der Vlist <kevin@kevinvandervlist.nl>
2012-03-06 20:55:45 +0100

    Proposed fix from mailinglist.

---
+++ bugs/bitmap_placement/fix.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import wx
+
+class Frame(wx.Frame):
+    def __init__(self):
+
+        title="bitmap placement issue"
+        pos=(0, 0)
+        size=(320, 240)
+
+        wx.Frame.__init__(self, parent=None, title=title, pos=pos, size=size)
+        p = wx.Panel(self)
+
+        bm = wx.BitmapFromImage(wx.Image("bitmap_placement.png", wx.BITMAP_TYPE_PNG))
+
+        self.bitmap = wx.StaticBitmap(parent=p, pos=pos, bitmap=bm, size=bm.Size)
+
+class App(wx.App):
+    def OnInit(self):
+        self.frame = Frame()
+        self.frame.Show()
+        return True
+
+if __name__ == '__main__':
+    app = App()
+    app.MainLoop()
